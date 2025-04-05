e5ef29e448466ce70538eb307a806f059d299407
milton-salgado <miltonsalgadoleandro@gmail.com>
2025-04-05 02:49:30 -0300

    update: Integração da página de Livros Doados com a API

--- src/pages/DonatedBooks/index.jsx
+++ src/pages/DonatedBooks/index.jsx
@@ -1,27 +1,43 @@
-import Book from "/assets/images/book-donated.png";
 import BookCard from "../../components/Card/BookCard";
-
+import axios from "axios";
+import { useState, useEffect } from "react";
 import style from "./donated-books.module.scss";
+import { showAlert } from "../../utils/helpers";
 
 const DonatedBooks = () => {
-    const donatedBooks = [
-        {
-            title: "O protagonista",
-            author: "Susanne Andrade",
-            genre: "Ficção",
-            image: {
-                src: Book,
-                alt: "Imagem de um livro doado",
-            },
-        },
-    ];
+    const [donatedBooks, setDonatedBooks] = useState([]);
+
+    const getDonatedBooks = async () => {
+        try {
+            const response = await axios.get(
+                "https://api-livros-vai-na-web-sn9x.onrender.com/donated-books"
+            );
+            setDonatedBooks(
+                Array.isArray(response.data.books) ? response.data.books : []
+            );
+            if (response.status === 200) {
+                showAlert("Livros doados carregados com sucesso!");
+            }
+        } catch (error) {
+            console.error("Error fetching donated books:", error);
+            if (error.response && error.response.status === 404)
+                showAlert("Erro: Nenhum livro encontrado.");
+            else if (error.response && error.response.status === 500)
+                showAlert("Erro: Problema no servidor.");
+            else showAlert("Erro: Não foi possível carregar os livros.");
+        }
+    };
+
+    useEffect(() => {
+        getDonatedBooks();
+    }, []);
 
     return (
         <main className={style.main}>
             <h1>Livros Doados</h1>
             <div className={style.cards}>
-                {donatedBooks.map((book, index) => (
-                    <BookCard key={index} book={book} />
+                {donatedBooks.map((book) => (
+                    <BookCard key={book.id} book={book} />
                 ))}
             </div>
         </main>
