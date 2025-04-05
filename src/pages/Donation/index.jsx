763b5ba2859c7c4c4603eb7a915a10a0d31c1578
milton-salgado <miltonsalgadoleandro@gmail.com>
2025-04-05 02:49:41 -0300

    update: Integração da página de Doação com a API

--- src/pages/Donation/index.jsx
+++ src/pages/Donation/index.jsx
@@ -1,10 +1,68 @@
 import DonateInput from "../../components/Input/DonateInput";
 import Book from "/assets/images/book.png";
 import Button from "../../components/Button";
+import axios from "axios";
+import { useState } from "react";
+import { showAlert } from "../../utils/helpers";
 
 import style from "./donation.module.scss";
 
 const Donation = () => {
+    const [title, setTitle] = useState("");
+    const [category, setCategory] = useState("");
+    const [author, setAuthor] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+
+    const sendData = async (e) => {
+        e.preventDefault();
+
+        const data = {
+            title,
+            category,
+            author,
+            image_url: imageUrl,
+        };
+
+        try {
+            const response = await axios.post(
+                "https://api-livros-vai-na-web-sn9x.onrender.com/donate",
+                data
+            );
+            resetForm();
+            if (response.status === 201) showAlert("Livro doado com sucesso!");
+        } catch (error) {
+            console.error("Error sending data:", error);
+            if (error.response && error.response.status === 400)
+                showAlert("Erro: Dados inválidos.");
+            else if (error.response && error.response.status === 500)
+                showAlert("Erro: Problema no servidor.");
+            else showAlert("Erro: Não foi possível enviar os dados.");
+        }
+    };
+
+    const handleTitleChange = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const handleCategoryChange = (e) => {
+        setCategory(e.target.value);
+    };
+
+    const handleAuthorChange = (e) => {
+        setAuthor(e.target.value);
+    };
+
+    const handleImageUrlChange = (e) => {
+        setImageUrl(e.target.value);
+    };
+
+    const resetForm = () => {
+        setTitle("");
+        setCategory("");
+        setAuthor("");
+        setImageUrl("");
+    };
+
     return (
         <main className={style.main}>
             <h1>
@@ -12,7 +70,7 @@ const Donation = () => {
                 informações do Livro
             </h1>
 
-            <form className={style.form}>
+            <form className={style.form} onSubmit={(e) => e.preventDefault()}>
                 <div className={style.title}>
                     <figure>
                         <img src={Book} alt="Imagem de um livro" />
@@ -27,6 +85,8 @@ const Donation = () => {
                         placeholder="Título"
                         required
                         variant={"donate"}
+                        onChange={handleTitleChange}
+                        value={title}
                     />
 
                     <DonateInput
@@ -34,6 +94,8 @@ const Donation = () => {
                         placeholder="Categoria"
                         required
                         variant={"donate"}
+                        onChange={handleCategoryChange}
+                        value={category}
                     />
 
                     <DonateInput
@@ -41,6 +103,8 @@ const Donation = () => {
                         placeholder="Autor"
                         required
                         variant={"donate"}
+                        onChange={handleAuthorChange}
+                        value={author}
                     />
 
                     <DonateInput
@@ -48,11 +112,13 @@ const Donation = () => {
                         placeholder="Link da imagem"
                         required
                         variant={"donate"}
+                        onChange={handleImageUrlChange}
+                        value={imageUrl}
                     />
                 </div>
 
                 <div className={style.buttonsWrapper}>
-                    <Button text="Doar" type="submit" />
+                    <Button text="Doar" type="submit" onClick={sendData} />
                 </div>
             </form>
         </main>
